6a5d825c65f4b03995e5fc05d9c4fb46669785a3
Maksymilian Czernecki <maksymilian.czernecki@gmail.com>
2020-06-08 17:39:07 +0200

    fix

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -30,25 +30,23 @@ CREATE TABLE "employee" (
     "position" INT NOT NULL,
     FOREIGN KEY (position) REFERENCES position(id)
 );
---
 
 CREATE TABLE "loyalty" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "user" VARCHAR NOT NULL,
-    "points" INT NOT NULL,
-);
+    "points" INT NOT NULL);
 
 CREATE TABLE "cart" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "product" INT NOT NULL,
     "user" VARCHAR NOT NULL,
-    FOREIGN KEY (product) REFERENCES product(id),
+    FOREIGN KEY (product) REFERENCES product(id)
 );
 
 CREATE TABLE "payment" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "amount" INT NOT NULL,
-    "accountNumber" TEXT NOT NULL,
+    "accountNumber" TEXT NOT NULL
 );
 
 CREATE TABLE "order" (
@@ -57,7 +55,7 @@ CREATE TABLE "order" (
     "cart" INT NOT NULL,
     "payment" INT NOT NULL,
     FOREIGN KEY (cart) REFERENCES cart(id),
-    FOREIGN KEY (payment) REFERENCES payment(id),
+    FOREIGN KEY (payment) REFERENCES payment(id)
 );
 
 
